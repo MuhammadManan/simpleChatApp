ba0503e1037a93338737baeb7fef989887559bb7
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-07-31 10:26:08 +0500

    making a small chat app

---
+++ socketIO/chat.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+const expressServer = app.listen(5000, () => {    
+    console.log('Server running on http://localhost:5000/');
+});
+const io = socketIO(expressServer);
+
+io.on('connection', (socket,req) => {  
+    console.log('a user connected');
+    socket.emit('welcome', 'Hello from server');
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg.message);
+    }); 
+}); 
--- socketIO/server.js
+++ socketIO/server.js
@@ -1,22 +1,22 @@
-const http = require('http');
-const socketIO = require('socket.io');
+// const http = require('http');
+// const socketIO = require('socket.io');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-const io = socketIO(server);
+// const io = socketIO(server);
 
-io.on('connection', (socket,req) => {
-    console.log('a user connected');
-    socket.emit('welcome', 'Hello from server');
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg.message);
-    });
-})
+// io.on('connection', (socket,req) => {
+//     console.log('a user connected');
+//     socket.emit('welcome', 'Hello from server');
+//     socket.on('message', (msg) => {
+//         console.log('message: ' + msg.message);
+//     });
+// })
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/');
-});
+// server.listen(3500, () => {
+//   console.log('Server running on http://localhost:3000/');
+// });
