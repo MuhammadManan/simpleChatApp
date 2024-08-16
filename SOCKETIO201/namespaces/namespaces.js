53974848742d47acc8b3339b920adfbe358ffe76
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-17 00:09:38 +0500

    complete the let me slack it namespace room

--- SOCKETIO201/namespaces/namespaces.js
+++ SOCKETIO201/namespaces/namespaces.js
@@ -5,17 +5,31 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-const expressServer = app.listen(3000, () => {
-  console.log('Server running on port 3000');
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+const expressServer = app.listen(8001, () => {
+  console.log('Server running on port 8001');
 });
 
 const io = socketio(expressServer);
 
-io.on('connection', (socket) => {
+io.of('/').on('connection', (socket) => {
+// io.on('connection', (socket) => {
+  socket.join('chatRoom');
+  io.of('/').to('chatRoom').emit('WelcomeToChatRoom',{});
+  io.of('/').to('chatRoom').to('chatRoom2').to('admin').emit('WelcomeToChatRoom',{});
+  io.of('/admin').emit('welcome','');
     console.log(socket.id, ' has connected');
-
      socket.on('newMessageToServer', (dataFromClient) => {
         console.log('Data from client: ', dataFromClient);
-        io.emit('newMessageToClients', {text: dataFromClient.text});
+        io.of('/').emit('newMessageToClients', {text: dataFromClient.text});
+        // io.emit('newMessageToClients', {text: dataFromClient.text});
     });
+});
+
+io.of('/admin').on('connection', (socket) => {
+    console.log('Someone connected to the admin namespace');
+    io.of('/admin').emit('welcomeToChatRoom', 'Welcome to the admin channel');
 });
