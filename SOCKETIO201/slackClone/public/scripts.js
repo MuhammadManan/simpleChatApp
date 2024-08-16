c1f33426841dfdaf293487f01a1e02a90bbad4d4
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-16 16:07:44 +0500

    add rooms to DOM

--- SOCKETIO201/slackClone/public/scripts.js
+++ SOCKETIO201/slackClone/public/scripts.js
@@ -17,11 +17,28 @@ socket.on('welcome',(data)=>{
 
 // listen the nsList from the server which is an array of namespaces
 socket.on('nsList',(nsData)=>{
-    console.log('The list of namespaces has arrived', nsData);
+    // console.log('The list of namespaces has arrived', nsData);
     let namespacesDiv = document.querySelector('.namespaces');
     namespacesDiv.innerHTML = "";
     nsData.forEach((ns)=>{
-        namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.name}><img src="${ns.img}" /></div>`;
+        namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}" /></div>`;
     });
 
-})
+    // Add a click listener for each namespace
+    Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
+        // console.log(elem);
+        elem.addEventListener('click',(e)=>{
+            const nsEndpoint = elem.getAttribute('ns');
+            // console.log(`${nsEndpoint} I should go to now`);
+            const clickedNs = nsData.find(row=> row.endpoint === nsEndpoint);
+            // console.log('clickedNs ',clickedNs);
+            const rooms = clickedNs.rooms;
+            // console.log('roomList',rooms);
+            let roomList = document.querySelector('.room-list');
+            roomList.innerHTML = "";
+            rooms.forEach((room)=>{
+                roomList.innerHTML += `<li><span class="glyphicon glyphicon-lock"></span>${room.roomName}</li>`
+            });
+        });
+    });    
+});
