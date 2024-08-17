1448b3241b619560c0b613d0064e154af27627e9
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-17 20:19:42 +0500

    setUp the folder structure of DOM for the Slack 201 Project

--- V2Code/SocketIO201/slack/slack.js
+++ V2Code/SocketIO201/slack/slack.js
@@ -4,7 +4,9 @@ const socketio = require('socket.io')
 
 app.use(express.static(__dirname + '/public'));
 
-const expressServer = app.listen(9000);
+const expressServer = app.listen(9000, ()=>{
+    console.log("Server is listening on port 9000")
+});
 const io = socketio(expressServer);
 // io.on = io.of('/').on
 io.on('connection',(socket)=>{
