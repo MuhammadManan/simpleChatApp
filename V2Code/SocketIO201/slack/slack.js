f22d94113d4d6e91831ef84e14fea064377c3eba
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-17 22:11:33 +0500

    setting up data and classes

--- V2Code/SocketIO201/slack/slack.js
+++ V2Code/SocketIO201/slack/slack.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const namespaces = require('./data/namespaces');
+
+console.log(namespaces[0]);
 
 app.use(express.static(__dirname + '/public'));
 
