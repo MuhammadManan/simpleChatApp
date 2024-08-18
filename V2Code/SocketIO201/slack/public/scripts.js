d0d7f54b7e6453af84c138e77865e2a8714caaa2
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-18 06:25:44 +0500

    Refactoring and Reorganzing our code

--- V2Code/SocketIO201/slack/public/scripts.js
+++ V2Code/SocketIO201/slack/public/scripts.js
@@ -2,11 +2,6 @@
 
 const socket = io('http://localhost:9000'); // the / namespace/endpoint
 
-// console.log(socket);
-console.log(socket.io);
-socket.on('connect',()=>{
-    console.log(socket.id)
-})
 
 socket.on('nsList',(nsData)=>{
     console.log("The list of namespaces has arrived!");
@@ -23,44 +18,7 @@ socket.on('nsList',(nsData)=>{
         });
     });
 
-    const nsSocket = io('http://localhost:9000/wiki');
-    nsSocket.on('nsRoomLoad',(nsRooms)=>{
-        console.log(nsRooms);
-
-        const roomList = document.querySelector('.room-list');
-        roomList.innerHTML = "";
-        nsRooms.forEach((room)=>{
-            let glyph;
-            if(room.privateRoom){
-                glyph = 'lock';
-            }  else{
-                glyph = 'globe';
-            }
-            roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
-        });
-
-        const roomNodes = document.getElementsByClassName('room');
-        Array.from(roomNodes).forEach((elem)=>{
-            elem.addEventListener('click',(e)=>{
-                console.log("Someone clicked on ", e.target.innerText);
-            });
-        });
-    });
+    joinNS('/wiki');
 });
 
-socket.on('messageFromServer',(dataFromServer)=>{
-    console.log(dataFromServer);
-    socket.emit('dataToServer',{data: "Data from the Client!"})
-})
-
-document.querySelector('#message-form').addEventListener('submit',(event)=>{
-    event.preventDefault();
-    const newMessage = document.querySelector('#user-message').value;
-    socket.emit('newMessageToServer',{text: newMessage})
-})
-
-socket.on('messageToClients',(msg)=>{
-    console.log(msg)
-    document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`
-})
 
