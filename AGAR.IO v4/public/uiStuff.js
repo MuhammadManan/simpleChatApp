f21b0b92a0d76b393b40741c802b31636ce07f82
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-24 16:20:27 +0500

    A few UI loose ends

--- AGAR.IO v4/public/uiStuff.js
+++ AGAR.IO v4/public/uiStuff.js
@@ -20,9 +20,18 @@ window.addEventListener('load', () => {
 document.querySelector('.name-form').addEventListener('submit', (e) => {
     e.preventDefault();
     player.name = document.querySelector('#name-input').value;
+    document.querySelector('.player-name').innerText = player.name;
     loginModal.hide();
     spawnModal.show();
     console.log(player);
 });
 
 
+document.querySelector('.start-game').addEventListener('click', (e) => {
+    spawnModal.hide();
+    const elArray = Array.from(document.querySelectorAll('.hiddenOnStart'));
+    elArray.forEach((el) => {
+        el.removeAttribute('hidden');
+    });
+    init();
+});
