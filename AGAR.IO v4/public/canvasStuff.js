81464bdf51de6f161bb19ba63f7330dd52a46363
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-24 19:26:57 +0500

    draw the player

--- AGAR.IO v4/public/canvasStuff.js
+++ AGAR.IO v4/public/canvasStuff.js
@@ -1,3 +1,19 @@
 const init = () => {
     console.log('init');
+    draw();
 }
+
+let randomX = Math.floor(500 * Math.random() + 50);
+let randomY = Math.floor(500 * Math.random() + 50);
+context.beginPath();
+context.fillStyle = "rgb(255, 0, 0)";
+context.arc(randomX, randomY, 10, 0, Math.PI * 2);
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = "rgb(0, 255, 0)";
+context.stroke();
+
+
+const draw = () => {
+    console.log('draw');
+};
