63e5d79dc4e46c9208403675ff01bb7e9b60f3bf
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-25 18:01:28 +0500

    Tock event sending player direction from client to server

--- AGAR.IO v4/public/socketStuff.js
+++ AGAR.IO v4/public/socketStuff.js
@@ -5,6 +5,13 @@ const init = async() => {
         const initOrbs = await socket.emitWithAck('init', {
              playerName: player.name
          });
+
+         setInterval(() => {
+            socket.emit('tock',{
+                xVector: player.xVector,
+                yVector: player.yVector
+            });
+        }, 33);
          console.log(initOrbs);
          orbs = initOrbs;
          draw();
--- AGAR.IO v4/socketStuff/socketMain.js
+++ AGAR.IO v4/socketStuff/socketMain.js
@@ -25,6 +25,8 @@ initGame();
 
 io.on('connect', (socket) => {
     // console.log('socket connected');
+    let player = {};
+
     socket.on('init', (playerObj, ackCallback) => {
 
         if(players.length === 0){
@@ -40,10 +42,25 @@ io.on('connect', (socket) => {
         const playerName = playerObj.playerName;
         const playerConfig = new PlayerConfig(settings);
         const playerData = new PlayerData(playerName, settings);
-        const player = new Player(socket.id, playerConfig, playerData);
+        player = new Player(socket.id, playerConfig, playerData);
         players.push(player);
     
         ackCallback(orbs);
+    });
+
+    socket.on('tock', (data) => {
+        speed = player.playerConfig.speed;
+        const xV = player.playerConfig.xVector = data.xVector;
+        const yV = player.playerConfig.yVector = data.yVector;
+
+        if((player.playerData.locX < 5 && xV < 0) || (player.playerData.locX > 500) && (xV > 0)){
+            player.playerData.locY -= speed * yV;
+        }else if((player.playerData.locY < 5 && yV > 0) || (player.playerData.locY > 500) && (yV < 0)){
+            player.playerData.locX += speed * xV;
+        }else{
+            player.playerData.locX += speed * xV;
+            player.playerData.locY -= speed * yV;
+        }
     });
 
     socket.on('disconnect', () => {
