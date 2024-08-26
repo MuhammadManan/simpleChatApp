dcaa346a8814246bb1d6543703838d89eeadf9ae
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-08-26 23:21:30 +0500

    Getting performance data part2

--- PerfLoad_V2/nodeClient/index.js
+++ PerfLoad_V2/nodeClient/index.js
@@ -1,13 +1,75 @@
 const os = require('os');
 const cpus = os.cpus();
-const freeMem = os.freemem();
-const totlaMem = os.totalmem();
-const usedMem = totlaMem - freeMem;
-const memUsage = Math.floor(usedMem / totlaMem * 100)/100;
-const osType = os.type();
-const upTime = os.uptime();
-const cpuModel = cpus[0].model;
-const numCores = cpus.length;
-const cpuSpeed = cpus[0].speed;
-console.log(osType);
-// console.log(os.userInfo());
+
+function performanceData() {
+    return new Promise(async (resolve, reject) => {
+        try{
+            const freeMem = os.freemem();
+        const totlaMem = os.totalmem();
+        const usedMem = totlaMem - freeMem;
+        const memUsage = Math.floor(usedMem / totlaMem * 100)/100;
+        const osType = os.type();
+        const upTime = os.uptime();
+        const cpuModel = cpus[0].model;
+        const numCores = cpus.length;
+        const cpuSpeed = cpus[0].speed;
+        const cpuLoad = await getCpuLoad();
+        resolve({
+            freeMem,
+            totlaMem,
+            usedMem,
+            memUsage,
+            osType,
+            upTime,
+            cpuModel,
+            numCores,
+            cpuSpeed,
+            cpuLoad
+        });
+        } catch(e){
+            reject("Rejected promise from Performance Data " + e);
+        }
+    });
+}
+
+ 
+
+
+function cpuAverage() {
+    const cpus = os.cpus();
+    let idleMs = 0;
+    let totalMs = 0;
+    cpus.forEach((aCore) => {
+        for (type in aCore.times) {
+            totalMs += aCore.times[type];
+        }
+        idleMs += aCore.times.idle;
+    });
+    return {
+        idle: idleMs / cpus.length,
+        total: totalMs / cpus.length
+    }
+}
+
+function getCpuLoad(){
+    const start = cpuAverage();
+    return new Promise((resolve, reject) => {
+        try{
+            setTimeout(() => {
+                const end = cpuAverage();
+                const idleDifference = end.idle - start.idle;
+                const totalDifference = end.total - start.total;
+                const percentageCpu = 100 - Math.floor(100 * idleDifference / totalDifference);
+                resolve(percentageCpu);
+            }, 100);
+        } catch(e){
+            reject("Rejected promise from getCpuLoad" + e);
+        }
+    });
+}
+
+performanceData().then((allData) => {
+    console.log(allData);
+}).catch((e) => {
+    console.log(e);
+});
